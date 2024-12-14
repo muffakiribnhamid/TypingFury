708f4db37ce2bca4f48ef0a89e502b489a08068a
Muffakir Hamid <muffakir63@gmail.com>
2024-12-14 17:45:11 +0530

    Final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
 
   ],
   theme: {
-    extend: {
-      keyframes: {
-        shake: {
-          '0%, 100%': { transform: 'translateX(0)' },
-          '25%': { transform: 'translateX(-5px)' },
-          '75%': { transform: 'translateX(5px)' },
-        }
-      },
-      animation: {
-        'shake': 'shake 0.2s ease-in-out 0s 3',
-      }
-    }
+    extend: {},
   },
   plugins: [],
 }
